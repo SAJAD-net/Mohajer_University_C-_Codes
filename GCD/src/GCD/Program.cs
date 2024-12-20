3abd1b6a3ac5a6ae8bec60b7b265a93eb509e75f
SAJADnet <sajadsajad.sch@gmail.com>
2024-12-20 04:10:25 +0330

    Adding GCD project

---
+++ GCD/src/GCD/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GCD
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.Write("- Enter the 1st number: ");
+            int first_num = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("- Enter the 2nd number: ");
+            int second_num = Convert.ToInt32(Console.ReadLine());
+
+            int gcd = GCD(first_num, second_num);
+
+            Console.WriteLine($"\n- {first_num} and {second_num}'s GCD is {gcd} !");
+
+            Console.ReadKey();
+        }
+
+
+        static int GCD(int first_num, int second_num)
+        {
+            while (second_num != 0)
+            {
+                int temp = second_num;
+                second_num = first_num % second_num;
+                first_num = temp;
+            }
+
+            return first_num;
+        }
+    }
+}
