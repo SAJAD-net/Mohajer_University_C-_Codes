3e92683030dbd5a52ac587b969acff7ab0b0282e
SAJADnet <sajadsajad.sch@gmail.com>
2024-12-23 06:07:49 +0330

    Adding xor_function

---
+++ xor_function/xor_function/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace xor_function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            bool b = true;
+
+            bool result = XOR(ref a, b);
+
+            Console.WriteLine(a);
+            Console.WriteLine($"a XOR b : {result}");
+
+            Console.ReadKey();
+        }
+
+        static bool XOR(ref bool c, bool d)
+        {
+            c = false;
+
+            if (c==d)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
