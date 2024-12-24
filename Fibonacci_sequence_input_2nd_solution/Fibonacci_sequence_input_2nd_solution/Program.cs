c935da4195785323b04cb4a5d29aed89928efc6c
SAJADnet <sajadsajad.sch@gmail.com>
2024-12-24 13:29:42 +0330

    Adding Fibonacci_sequence_input_2nd_solution project

---
+++ Fibonacci_sequence_input_2nd_solution/Fibonacci_sequence_input_2nd_solution/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fibonacci_sequence_input_2nd_solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of Fibonacci sequence to generate: ");
+            int count = Convert.ToInt32(Console.ReadLine());
+            
+            int a = 0;
+            int b = 1;
+            int fib = 0;
+            Console.Write("{0} {1} ", 0, 1);
+
+            for (int i = 2; i < 20; i++)
+            {
+                fib = a + b;
+                Console.Write("{0} ", fib);
+                a = b;
+                b = fib;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
