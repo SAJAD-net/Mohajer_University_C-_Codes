c266b67a19612c8e0e7860b6a5602b2c69963613
SAJADnet <sajadsajad.sch@gmail.com>
2024-12-24 13:29:06 +0330

    Adding Fibonacci_sequence_input project

---
+++ Fibonacci_sequence_input/Fibonacci_sequence_input/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fibonacci_sequence_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of Fibonacci sequence to generate: ");
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("{0} ", Calculate(i));
+            }
+
+            Console.ReadKey();
+        }
+
+        public static int Calculate(int n)
+        {
+            if (n < 2)
+                return n;
+
+            else
+                return Calculate(n - 1) + Calculate(n - 2);
+
+        }
+    }
+}
