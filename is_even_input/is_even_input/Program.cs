9a29691d877d9d93023eed7728e4d5aaddecf616
SAJADnet <sajadsajad.sch@gmail.com>
2024-12-24 13:33:42 +0330

    Adding is_even_input project

---
+++ is_even_input/is_even_input/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace is_even_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("{0} is an even number !", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is an odd number !", num);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
