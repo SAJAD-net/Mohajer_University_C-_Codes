07775d1f0b1e94a282a10938ba52bc8224c2b3f1
SAJADnet <sajadsajad.sch@gmail.com>
2024-12-24 14:37:56 +0330

    Adding matrix_multiplication project

---
+++ matrix_multiplication/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matrix1 = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        int[,] matrix2 = {
+            { 9, 8, 7 },
+            { 6, 5, 4 },
+            { 3, 2, 1 }
+        };
+
+        Console.WriteLine("Matrix 1:");
+        PrintMatrix(matrix1);
+
+
+        Console.WriteLine("\nMatrix 2:");
+        PrintMatrix(matrix2);
+
+
+        int[,] resultMatrix = MultiplyMatrices(matrix1, matrix2);
+
+
+        Console.WriteLine("\nResult of Multiplication:");
+        PrintMatrix(resultMatrix);
+    }
+
+    static void PrintMatrix(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static int[,] MultiplyMatrices(int[,] matrix1, int[,] matrix2)
+    {
+        int rows1 = matrix1.GetLength(0);
+        int cols1 = matrix1.GetLength(1);
+        int rows2 = matrix2.GetLength(0);
+        int cols2 = matrix2.GetLength(1);
+
+        if (cols1 != rows2)
+            throw new InvalidOperationException("Cannot multiply matrices: incompatible dimensions.");
+
+        int[,] result = new int[rows1, cols2];
+
+        for (int i = 0; i < rows1; i++)
+        {
+            for (int j = 0; j < cols2; j++)
+            {
+                result[i, j] = 0;
+                for (int k = 0; k < cols1; k++)
+                {
+                    result[i, j] += matrix1[i, k] * matrix2[k, j];
+                }
+            }
+        }
+
+        return result;
+    }
+}
